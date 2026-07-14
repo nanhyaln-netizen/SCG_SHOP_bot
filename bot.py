630128d0534ce639be98245c3eb43b595a02bcb9
nanhyaln-netizen <nanhyaln@gmail.com>
2026-07-14 23:20:41 +0630

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,6 +4,8 @@ from telegram.ext import Application, CommandHandler, MessageHandler, filters, C
 
 TOKEN = os.getenv("BOT_TOKEN")
 
+ADMIN_CHAT_ID = 8758621220
+
 PAYMENT = """
 💳 Payment
 
@@ -29,6 +31,16 @@ Order တင်ရန် ပို့ပေးပါ—
 """)
 
 async def reply_order(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user = update.effective_user
+    text = update.message.text
+
+    # မင်းဆီ Order ပို့
+    await context.bot.send_message(
+        chat_id=ADMIN_CHAT_ID,
+        text=f"📩 New Order\n\n👤 {user.full_name}\n🆔 {user.id}\n\n{text}"
+    )
+
+    # Customer ကို Reply
     await update.message.reply_text(
         "✅ Order လက်ခံရရှိပါပြီ\n"
         "စစ်ဆေးပြီး အမြန်ဆုံး ပြန်လည်ဆောင်ရွက်ပေးပါမယ်။\n\n"
@@ -39,10 +51,9 @@ def main():
     app = Application.builder().token(TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
-    app.add_handler(MessageHandler(filters.TEXT, reply_order))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, reply_order))
 
     app.run_polling()
 
 if __name__ == "__main__":
     main()
-
